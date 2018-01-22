5b6754e171a3162a9f62235a8a1f4ce519ae5c84
Abhishek Sharma <absharma6561@gmail.com>
2018-01-21 18:40:53 -0800

    CMS--Beauty-Spot-Salon

---
+++ includes/layouts/footer.php
@@ -0,0 +1,10 @@
+    <div id="footer">Designed by Abhishek Sharma</div>
+
+	</body>
+</html>
+<?php
+  // 5. Close database connection
+	if (isset($connection)) {
+	  mysqli_close($connection);
+	}
+?>
